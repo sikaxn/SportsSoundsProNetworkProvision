a99d5f29ca5e9b9e4be8f8e650361a47a1201163
Fennekin The Cute Fox <15jz38@queensu.ca>
2020-11-25 19:29:03 -0500

    Update for features
    -Icon for Start SSP now become the actual SSP icon when provisioning.
    -Command prompt window now minimize itself after launching SSP
    -Command prompt winodo now echo current log and use a better way to detect weather SSP is running so it doesn't get stuck sometimes.

--- SSP_LocalProvision_Log.bat
+++ SSP_LocalProvision_Log.bat
@@ -18,7 +18,9 @@ SET LOG=".\%~N0_runtime.log"
   echo sLinkFile = oWS.ExpandEnvironmentStrings^("!Esc_LinkDest!"^)
   echo Set oLink = oWS.CreateShortcut^(sLinkFile^) 
   echo oLink.TargetPath = oWS.ExpandEnvironmentStrings^("!Esc_LinkTarget!"^)
+  echo oLink.IconLocation = "%AppData%\SportsSoundsPro\SportsSoundsPro.exe"
   echo oLink.Save
+  
 )1>!cSctVBS!
 cscript //nologo .\!cSctVBS!
 DEL !cSctVBS! /f /q
--- SSP_start.bat
+++ SSP_start.bat
@@ -1,6 +1,7 @@
+@echo off
 net use z: /delete
 echo Mapping Network Drive
-net use z: \server\music
+net use z: \\server\music
 
 
 echo Starting SSP
@@ -9,18 +10,37 @@ SETLOCAL EnableExtensions
 set EXE=SportsSoundsPro.exe
 FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% goto FOUND
 echo Not running
-goto FIN
+goto NOTFOUND
 :FOUND
 echo Running
 mshta "javascript:new ActiveXObject('WScript.Shell').popup('SSP is already running.',7,'ERROR',64);window.close();"
 exit
 
-:FIN
+:NOTFOUND
 mshta "javascript:new ActiveXObject('WScript.Shell').popup('DO NOT CLOSE Command Pronpt. This can ensure logs being collected and uploaded to the server.',7,'Logit warning',64);window.close();"
-start /w %AppData%\SportsSoundsPro\SportsSoundsPro.exe 
+start %AppData%\SportsSoundsPro\SportsSoundsPro.exe 
+powershell -window minimized -command ""
+
+:CHECK
+echo SSP is running DONOT CLOSE
+more %AppData%\SportsSoundsPro\SportsSoundsProLog.txt
+@timeout /t 3 
+cls
+FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF %%x == %EXE% goto CHECK
+
+echo QUITTING
+timeout 1
+goto QUITTING
+:QUITTING
 echo Uploading logs
 xcopy /K /H /Y /D %AppData%\SportsSoundsPro\SportsSoundsProLog.txt Z:\SSPLogFile\
 rename Z:\SSPLogFile\SportsSoundsProLog.txt "SSPLog_%date:~0,4%-%date:~5,2%-%date:~8,2%_%time:~0,2%-%time:~3,2%-%time:~6,2%.txt"
 del /f %AppData%\SportsSoundsPro\SportsSoundsProLog.txt
 xcopy /K /H /Y /D %AppData%\SportsSoundsPro\SSP.inf Z:\SportsSoundsPro\SSP.inf 
 echo Done
+echo  
+echo Disconnecting from server
+net use z: /delete
+echo Window will close
+timeout 3
+exit
